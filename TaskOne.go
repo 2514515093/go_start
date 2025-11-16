0fbf4bd1d8d30e51ae18443ef0220a0153a24e2c
yjl <2514515093@qq.com>
2025-11-16 13:18:51 +0800

    -

--- TaskOne.go
+++ TaskOne.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 )
 
@@ -10,7 +11,10 @@ func main() {
 	//two()
 	//three()
 	//four()
-	five()
+	//five()
+	//six()
+	//seven()
+	eight()
 }
 
 func one() {
@@ -119,6 +123,78 @@ func five() {
 		ndigits[len(digits)-1] = ndigits[len(digits)-1] + 1
 	}
 	fmt.Println(ndigits)
+}
+
+func six() {
+	//给你一个有序数组 nums ，请你原地删除重复出现的元素，使每个元素只出现一次，返回删除后数组的新长度。
+	//不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。可以使用双指针法，
+	//一个慢指针 i 用于记录不重复元素的位置，一个快指针 j 用于遍历数组，当 nums[i] 与 nums[j] 不相等时，将 nums[j] 赋值给 nums[i + 1]，并将 i 后移一位。
+	nums := []int{1, 2, 3, 3, 4, 5, 5, 6}
+	i := 0
+	for j, n := range nums {
+		if j == 0 {
+			continue
+		}
+		if nums[i] != n {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	fmt.Println(i + 1)
+}
+
+func seven() {
+	//以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。
+	//请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+	//可以先对区间数组按照区间的起始位置进行排序，然后使用一个切片来存储合并后的区间，遍历排序后的区间数组，
+	//将当前区间与切片中最后一个区间进行比较，如果有重叠，则合并区间；如果没有重叠，则将当前区间添加到切片中。
+	intervals := [][]int{{1, 3}, {2, 4}, {8, 10}, {15, 18}}
+	sort.Slice(intervals, func(a, b int) bool {
+		return intervals[a][0] < intervals[b][0]
+	})
+	nintervals := [][]int{}
+	a := 0
+	for i, v := range intervals {
+		if i == 0 {
+			nintervals = append(nintervals, v)
+			continue
+		}
+		if nintervals[a][1] >= v[0] {
+			nintervals[a][1] = max(nintervals[a][1], v[1])
+		} else {
+			a++
+			nintervals = append(nintervals, v)
+		}
+	}
+	fmt.Println(nintervals)
+}
+
+func eight() {
+	// 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数
+	nums := []int{2, 7, 11, 15, 20}
+	target := 22
+	m := make(map[int]int)
+	for _, v := range nums {
+		cz := target - v
+		m[v] = cz
+	}
+	var a int
+	for k, v := range m {
+		if Contains(nums, v) && k != v {
+			a = k
+			break
+		}
+	}
+	fmt.Println(a, m[a])
+}
+
+func Contains[T comparable](nums []T, target T) bool {
+	for _, v := range nums {
+		if v == target {
+			return true
+		}
+	}
+	return false
 }
 
 func runeAt(s string, i int) string {
